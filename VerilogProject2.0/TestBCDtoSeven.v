490f01cc12d24b6e97dd4fa761f171637c091f25
Jesus A Guerra <jesus.guerra@ttu.edu>
2018-02-08 22:24:56 -0600

    Add files via upload

---
+++ VerilogProject2.0/TestBCDtoSeven.v
@@ -0,0 +1,31 @@
+//TestBCDtoSevenSegmentDisplay
+// Developed by: Jesus Adrian Guerra with help from Jeneane Amin and Rhyan Johnson
+
+//Tests BCDtoSevenSeg.v
+
+module Test_BCDtoSevenSeg();
+
+//Inputs
+reg [3:0] BCD;
+
+//Outputs
+wire [6:0] SEG;
+
+
+//Instantiate
+BCDtoSevenSeg uut(
+    .BCD(BCD),
+    .SEG(SEG)
+);
+
+//begin test
+initial begin
+  #100
+  BCD = 4'd1;
+#10 BCD = 4'd3;
+#10 BCD = 4'd6;
+#10 BCD = 4'd9;
+
+end
+
+endmodule
---
+++ VerilogProject2.0/TestBinarytoBCD.v
@@ -0,0 +1,37 @@
+// Developed by: Jesus Adrian Guerra with help from Jeneane Amin and Rhyan Johnson
+
+module TEST_binary_to_BCD;
+
+	// Inputs
+	reg [32:0] bnum;
+
+	// Outputs
+	wire [3:0] tens;
+	wire [3:0] ones;
+
+	// Instantiate the Unit Under Test (UUT)
+	BinaryToBCD uut (
+		.bnum(bnum), 
+		.tens(tens), 
+		.ones(ones),
+        .hundreds(hundreds)
+	);
+
+	initial begin
+		// Initialize Inputs
+		bnum = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		#10 bnum = 43;
+		
+		#10 bnum = 21;
+		
+		#10 bnum = 30;
+		
+		#100;
+
+	end
+      
+endmodule
